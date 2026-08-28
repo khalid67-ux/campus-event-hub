971c4371ea9f40bbd53beb627e0628d20069e3fc
khalid67-ux <mdk806124@gmail.com>
2026-08-28 10:49:38 +0600

    Create details.js

---
+++ js/details.js
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>Event Details | Campus Event Hub</title>
+
+    <link
+        rel="stylesheet"
+        href="css/style.css"
+    >
+
+</head>
+
+<body>
+
+<header class="navbar">
+
+    <div class="container nav-content">
+
+        <a href="index.html" class="logo">
+            Campus <span>Hub</span>
+        </a>
+
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="events.html">Events</a>
+            <a href="my-registration.html">
+                My Registration
+            </a>
+        </nav>
+
+    </div>
+
+</header>
+
+
+<main>
+
+<section class="section">
+
+<div class="container">
+
+    <div id="eventDetails"></div>
+
+</div>
+
+</section>
+
+</main>
+
+
+<footer class="footer">
+
+    <div class="container">
+
+        <p>
+            © 2026 Campus Event Hub.
+        </p>
+
+    </div>
+
+</footer>
+
+
+<script src="js/data.js"></script>
+<script src="js/details.js"></script>
+
+</body>
+
+</html>
